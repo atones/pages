dc150f29f316ce3b45b037cc1f2c1787d3f4c7ae
CodePsy-2001 <codepsy2001@gmail.com>
2024-08-07 10:46:17 +0900

    add rewrites

--- apps/home/next.config.mjs
+++ apps/home/next.config.mjs
@@ -3,7 +3,13 @@ import withMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  rewrites: async () => [
+    {
+      source: '/certcopy',
+      destination: 'https://atones-homepage-v2-atones.vercel.app',
+    }
+  ]
 }
 
 export default withMDX()(nextConfig)

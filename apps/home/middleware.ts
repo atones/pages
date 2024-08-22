90bf99b1659f674415f8f254a7af2cda8faa296b
CodePsy-2001 <codepsy2001@gmail.com>
2024-08-22 16:48:01 +0900

    edit middleware

--- apps/home/middleware.ts
+++ apps/home/middleware.ts
@@ -4,16 +4,13 @@ import { NextRequest } from 'next/server'
 const I18nMiddleware = createI18nMiddleware({
   locales: ['ko', 'en'],
   defaultLocale: 'ko',
-  urlMappingStrategy: 'rewriteDefault'
+  urlMappingStrategy: 'rewriteDefault',
+  resolveLocaleFromRequest: () => null
 })
 
 export function middleware (request: NextRequest) {
   return I18nMiddleware(request as unknown as never)
 }
-
-const imageExts = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp']
-const staticExts = ['xml', 'txt', 'json', 'ico']
-const exts = imageExts.concat(staticExts)
 
 export const config = {
   matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt|sitemap.xml).*)']
